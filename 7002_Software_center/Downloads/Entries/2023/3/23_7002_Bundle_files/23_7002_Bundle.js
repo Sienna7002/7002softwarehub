270fbb7c869e6bd29576b050c843ad4bbd40f5fb
7002_ <97623785+Sienna7002@users.noreply.github.com>
2023-03-23 17:28:53 +0000

    Delete 7002_Software_center directory

--- 7002_Software_center/Downloads/Entries/2023/3/23_7002_Bundle_files/23_7002_Bundle.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20230323
-
-setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{dynamicallyPopulate();loadMozillaCSS('23_7002_Bundle_files/23_7002_BundleMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
